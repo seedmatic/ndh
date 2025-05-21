nxmatic/nix-darwin-home

rebased on 24.11 darwin and added vmnet socket from lima

modules/darwin/default.nix
@@ -1,12 +1,13 @@
 {...}:
-builtins.trace "Evaluating darwin/default.nix" {
-  imports = builtins.map (module: builtins.trace "Importing ${module}" (import module)) [
+builtins.traceVerbose "Evaluating darwin/default.nix" {
+  imports = builtins.map (module: builtins.traceVerbose "Importing ${module}" (import module)) [
     ../common
     ./preferences.nix
     ./security.nix
     ./core.nix
     ./emacs.nix
     ./raycast.nix
+    ./socket_vmnet.nix
     ./syncthing.nix
     ./tailscale.nix
     ./homebrew.nix

seedmatic/ndh

modules/darwin/default.nix
@@ -1,14 +1,10 @@
 {...}:
-builtins.trace "Evaluating darwin/default.nix" {
-  imports = builtins.map (module: builtins.trace "Importing ${module}" (import module)) [
+builtins.traceVerbose "Evaluating darwin/default.nix" {
+  imports = builtins.map (module: builtins.traceVerbose "Importing ${module}" (import module)) [
     ../common
     ./preferences.nix
     ./security.nix
     ./core.nix
-    ./emacs.nix
-    ./raycast.nix
-    ./syncthing.nix
-    ./tailscale.nix
-    ./homebrew.nix
+    ./environment.nix
   ];
 }