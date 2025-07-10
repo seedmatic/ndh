eb358458abaf63c181db8b785b240a7a693ae55a
Stephane Lacoin (aka nxmatic) <stephane.lacoin@gmail.com>
2025-07-10 10:19:06 +0200

    fixup: do not hardcode use name in zshenv

--- modules/home-manager/shell/zshenv.zsh
+++ modules/home-manager/shell/zshenv.zsh
@@ -8,7 +8,8 @@ path=( /run/wrappers/bin "${path[@]}" )
 : ensure we\'re loading the rcs files
 declare -g ZDOTDIR=${HOME}/.config/zsh
 
-path=( /etc/profiles/per-user/nxmatic/bin /run/current-system/sw/bin "${path[@]}" )
+: ensure we\'re loading the wrappers and user bin directory at first
+path=( /run/sw/wrappers/bin "/etc/profiles/per-user/$USER/bin" /run/current-system/sw/bin "${path[@]}" )
 
 # ZDOTDEBUG=true
 
