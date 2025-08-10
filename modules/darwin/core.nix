3fb9a53fa9d3559d9822b03a7856cc4abe6a99e6
Stephane Lacoin (aka nxmatic) <stephane.lacoin@gmail.com>
2025-08-10 09:16:56 +0200

    wip

--- modules/darwin/core.nix
+++ modules/darwin/core.nix
@@ -15,10 +15,20 @@ in {
 
     extraOptions = ''
       accept-flake-config = true
-      experimental-features = nix-command flakes
-      extra-platforms = x86_64-darwin aarch64-darwin
+      extra-substituters = https://install.determinate.system
+      extra-trusted-public-keys = cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM= cache.flakehub.com-4:Asi8qIv291s0aYLyH6IOnr5Kf6+OF14WVjkE6t3xMio= cache.flakehub.com-5:zB96CRlL7tiPtzA9/WKyPkp3A2vqxqgdgyTVNGShPDU= cache.flakehub.com-6:W4EGFwAGgBj3he7c5fNh9NkOXw0PUVaxygCVKeuvaqU= cache.flakehub.com-7:mvxJ2DZVHn/kRxlIaxYNMuDG1OvMckZu32um1TadOR8= cache.flakehub.com-8:moO+OVS0mnTjBTcOUh2kYLQEd59ExzyoW1QgQ8XAARQ= cache.flakehub.com-9:wChaSeTI6TeCuV/Sg2513ZIM9i0qJaYsF+lZCXg0J6o= cache.flakehub.com-10:2GqeNlIp6AKp4EF2MVbE1kBOp9iBSyo0UPR9KoR0o1Y=
+
+      upgrade-nix-store-path-url = https://install.determinate.systems/determinate-nix/stable/fallback-paths.nix
+
+      extra-experimental-features = nix-command flakes
+      extra-platforms = aarch64-darwin
+      extra-nix-path = nixpkgs=flake:https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/*.tar.gz
+      always-allow-substituters = true
       min-free = ${toString (10 * 1024 * 1024 * 1024)}  # 10 GB
       max-free = ${toString (20 * 1024 * 1024 * 1024)}  # 20 GB
+      ssl-cert-file = /etc/nix/macos-keychain.crt
+
+      include flox.conf
     '';
 
     # Configure NIX_PATH for legacy nix commands and <nixpkgs> imports
--- modules/darwin/linux-builder.nix
+++ modules/darwin/linux-builder.nix
@@ -2,6 +2,10 @@
 let
   useCustomConfig = config.linux-builder.useCustomConfig;
   qemu-pkgdb = self.packages.${pkgs.system}.qemu-pkgdb or pkgs.qemu;
+
+  keys = lib.importYAML ./ssh.d/keys.yaml;
+  linuxBuilderCommittedPubKey = keys.profiles.committed.linux-builder.public; 
+  linuxBuilderCommittedWorkKey = keys.profiles.committed.linux-builder.work;
 in {
   options.linux-builder.useCustomConfig = lib.mkOption {
     type = lib.types.bool;
@@ -10,82 +14,23 @@ in {
   };
 
   config = {
-    # Ensure our shared SSH keys are used instead of generated ones
-    system.activationScripts.setupLinuxBuilderKey = {
-      text = ''
-        # Replace any auto-generated linux-builder keys with our shared key
-        if [ -f /etc/nix/linux-builder_ed25519 ]; then
-          cp ${../../keys/builder_ed25519} /etc/nix/linux-builder_ed25519
-          chmod 600 /etc/nix/linux-builder_ed25519
-          chown root:wheel /etc/nix/linux-builder_ed25519
-        fi
-        if [ -f /etc/nix/linux-builder_ed25519.pub ]; then
-          cp ${../../keys/builder_ed25519.pub} /etc/nix/linux-builder_ed25519.pub
-          chmod 644 /etc/nix/linux-builder_ed25519.pub
-          chown root:wheel /etc/nix/linux-builder_ed25519.pub
-        fi
-      '';
-      deps = [ "etc" ];  # Run after /etc files are set up
-    };
+    # SSH keys are managed by distributed-builds.nix at /etc/nix/builder_ed25519
     
     nix.linux-builder = {
       enable = true;
-      ephemeral = false;
+      ephemeral = true;
       maxJobs = 4;
-      supportedFeatures = [ "kvm" "benchmark" "big-parallel" ];
-      # Don't automatically register as build machine if distributed builds are enabled
-    } // lib.optionalAttrs (!config.services.crossHostBuilders.enable) {
-      # Only add to build machines if distributed builds are disabled
-      # (when distributed builds are enabled, they manage build machines exclusively)
-    } // lib.optionalAttrs useCustomConfig {
-      config = { pkgs, ... }:
-        let linuxPkgs = import <nixpkgs> { system = "aarch64-linux"; };
-        in {
-          nix.channel.enable = lib.mkForce true;
-
-          virtualisation = {
-            qemu.package = qemu-pkgdb; # May use the patched QEMU package
-            darwin-builder = {
-              diskSize = 200 * 1024;
-              memorySize = 8 * 1024;
-            };
-            cores = 6;
-          };
-
-          services.openssh = {
-            enable = true;
-            settings = {
-              PasswordAuthentication = false;
-              PermitRootLogin = "yes";
-              PermitEmptyPasswords = false;
-              PubkeyAuthentication = true;
-            };
-          };
-
-          users.users.root.password = "root";
-          users.users.builder = {
-            isNormalUser = true;
-            extraGroups = [ "wheel" ];
-            # Add SSH key for remote builds
-            openssh.authorizedKeys.keyFiles = [
-              ../../keys/builder_ed25519.pub
-            ];
-          };
-          users.users.test = {
-            isNormalUser = true;
-            password = "";
-          };
-
-          security.sudo.extraRules = [{
-            users = [ "%wheel" ];
-            commands = [{
-              command = "ALL";
-              options = [
-                "NOPASSWD"
-              ]; # "SETENV" # Adding the following could be a good idea
-            }];
-          }];
-        };
+      systems = [ "aarch64-linux" ];
+      protocol = "ssh-ng";
+      speedFactor = 1;
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      mandatoryFeatures = [];
+      config = {
+        virtualisation.darwin-builder.hostPort = 31022;
+        services.openssh.authorizedKeys.keys = [ linuxBuilderCommittedPubKey
+          linuxBuilderCommittedWorkKey
+        ];
+      };
     };
   };
 }
