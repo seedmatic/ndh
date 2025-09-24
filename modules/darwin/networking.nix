d659b35d474cd79528d1a0156272881d23b1aaad
Stephane Lacoin (aka nxmatic) <stephane.lacoin@gmail.com>
2025-09-24 12:53:54 +0200

    feat(darwin): ipv6 dns servers

--- modules/darwin/networking.nix
+++ modules/darwin/networking.nix
@@ -2,7 +2,13 @@
   config = {
     # Ensure all configuration attributes are within the config attribute
     networking = {
-      dns = [ "100.100.100.100" "8.8.8.8" "1.1.1.1" "1.0.0.1" "8.8.4.4" ];
+      dns = [ 
+        "100.100.100.100"
+         "8.8.8.8" "2001:4860:4860::8888" 
+         "1.1.1.1" "2606:4700:4700::1111" 
+         "1.0.0.1" 
+         "8.8.4.4" "2001:4860:4860::8844"
+      ];
       knownNetworkServices = config.profile.darwin.knownNetworkServices;
     };
   };
