8896443728a67af9e097a3a436cfe2bc4149d910
Stephane Lacoin (aka nxmatic) <stephane.lacoin@gmail.com>
2025-11-13 10:18:26 +0100

    feat(incus): add FHS environment wrapper for distrobuilder
    - Create buildFHSUserEnv wrapper for distrobuilder compatibility
    - Include all required tools for debootstrap image building
    - Update security wrappers to use FHS-wrapped version
    - Resolves distrobuilder execution issues in NixOS environment

--- modules/nixos/incus.nix
+++ modules/nixos/incus.nix
@@ -7,11 +7,36 @@ let
     find /run/incus -type d -exec chmod g+rwx {} +
   '';
 
+  # Create an FHS environment for distrobuilder/debootstrap to work properly
+  distrobuilderFHS = pkgs.buildFHSUserEnv {
+    name = "distrobuilder-fhs";
+    targetPkgs = pkgs: with pkgs; [
+      distrobuilder
+      debootstrap
+      dpkg
+      gnused
+      gnugrep
+      gnutar
+      gzip
+      gawk
+      util-linux
+      coreutils
+      findutils
+      bash
+      perl
+      wget
+      curl
+      cacert
+    ];
+    runScript = "distrobuilder";
+  };
+
 in {
   environment.systemPackages = with pkgs; [ 
     incus 
     incus-compose 
     skopeo 
+    distrobuilderFHS  # Use FHS-wrapped version instead of bare distrobuilder
     debootstrap
     dpkg
     # Additional tools needed by debootstrap for proper Debian image building
@@ -105,7 +130,7 @@ in {
   };
 
   security.wrappers.distrobuilder = {
-    source = "${pkgs.distrobuilder}/bin/distrobuilder";
+    source = "${distrobuilderFHS}/bin/distrobuilder-fhs";
     owner = "root";
     group = "incus";
     setuid = true;
