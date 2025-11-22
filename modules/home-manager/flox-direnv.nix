19184b4cb6881e1bbb562cbe9816b546f3f73f30
Stephane Lacoin (aka nxmatic) <stephane.lacoin@hyland.com>
2025-11-22 16:19:51 +0100

    feat: refactor direnv flox helper @codebase @copilot

--- modules/home-manager/flox-direnv.nix
+++ modules/home-manager/flox-direnv.nix
@@ -1,17 +1,12 @@
-{ lib, pkgs, ... }: let
-
-  dollar = "$";
-
+{ lib, pkgs, ... }:
+let
   use-flox-rc-path = pkgs.writeScript "direnv-use-flox.rc" (builtins.readFile ./direnv-use-flox.rc);
-  flox-rc-path = pkgs.writeScript "flox.rc.sh" (builtins.readFile ./flox.rc);
-
 in {
   programs.direnv = {
 
     stdlib = lib.mkAfter ''
       PATH_add /run/wrappers/bin
       PATH_add /run/current-system/sw/bin
-      export FLOX_RCPATH="${flox-rc-path}"
 
       source_env "${use-flox-rc-path}"
     '';
