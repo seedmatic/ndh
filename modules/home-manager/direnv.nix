9ecc3ec0286fc19a630c0eff6db45d52d143ec05
Stephane Lacoin (aka nxmatic) <stephane.lacoin@gmail.com>
2025-11-26 22:33:04 +0100

    direnv: add tracing helpers

--- modules/home-manager/direnv.nix
+++ modules/home-manager/direnv.nix
@@ -8,14 +8,30 @@ in {
     enableZshIntegration = true;
 
     stdlib = lib.mkBefore ''
+      # exec 5> $HOME/.local/direnv-bash.log
+      # BASH_XTRACEFD="5"
+      # PS4='[${dollar}{BASH_SOURCE[0]:-inherited}:${dollar}{LINENO}:${dollar}{FUNCNAME[0]:-main}] '
+      # set -x
+
       direnv_layout_dir() {
         local pwd_hash
         pwd_hash=${dollar}(basename "${dollar}PWD")-${dollar}(echo -n "${dollar}PWD" | shasum | cut -d ' ' -f 1 | head -c 7)
         echo "${dollar}XDG_CACHE_HOME/direnv/layouts/${dollar}pwd_hash"
       }
 
-      source_env_if_exists ${dollar}{BASH_SOURCE}~${dollar}(uname)
-      source_env_if_exists ${dollar}{BASH_SOURCE}~${dollar}(hostname)
+      my:source_env() {
+        local base_path="${dollar}{BASH_SOURCE}"
+        local suffix="$1"
+        local env_file="${dollar}{base_path}.${dollar}{suffix}"
+        if [ ! -f "$env_file" ]; then
+          return
+        fi
+        log_status "Sourcing environment file: $env_file"
+        source_env "$env_file"
+      }
+
+      my:source_env $(uname)
+      my:source_env $(hostname)
     '';
 
   };
