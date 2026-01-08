185f3df6d23d360942f10b3e958d2b3cb2309eb8
nxmatic <stephane.lacoin@gmail.com>
2026-01-08 10:38:57 +0100

    chore(cachix): trusted nxmatic cachix

--- hosts/bioskop/flake.nix
+++ hosts/bioskop/flake.nix
@@ -1,6 +1,18 @@
 {
   description = "nix system configurations for bioskop";
 
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nxmatic.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nxmatic.cachix.org-1:huMghYiwDpPa1PMXHXK4G1Dp4QOZjgsNqxcjf/AjuJ0="
+    ];
+  };
+
   inputs = {
     nix-darwin-home.url = "path:../..";
   };
