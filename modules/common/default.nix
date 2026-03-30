822645f5f30f7900f76f44d587bd86a07a759d3b
nxmatic <stephane.lacoin@gmail.com>
2026-03-30 16:42:44 +0200

    fixup! wrong user home in darwin modules

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -13,7 +13,7 @@ let
   profile = cfg;
   user = cfg.user;
   userName = user.name;
-  userHome = "${if pkgs.stdenvNoCC.isDarwin then "/Users" else "/home"}/${userName}";
+  userHome = toString cfg.user.home;
   activationLogFile = "${userHome}/.local/state/nix/activation.log";
   hmActivationPackage = lib.attrByPath [
     "home-manager"
