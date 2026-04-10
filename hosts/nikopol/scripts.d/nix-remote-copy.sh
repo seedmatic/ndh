d578e740ff1d736ba100a934f62537841d042cd5
nxmatic <stephane.lacoin@gmail.com>
2026-04-10 02:12:37 +0200

    feat(nix): helper script for vz-host.nikopol mainly

--- hosts/nikopol/scripts.d/nix-remote-copy.sh
+++ hosts/nikopol/scripts.d/nix-remote-copy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env -S bash -exuo pipefail
 
 host=$1; shift
-exec nix copy --to "ssh-ng://${host}?remote-program=/nix/var/nix/profiles/default/bin/nix-daemon" $*
+out=$( nix build .#homeManagerConfiguration.activationPackage --json --no-link |
+       yq -pjson '.[0].outputs.out' )
+exec nix copy --to "ssh-ng://${host}?remote-program=/nix/var/nix/profiles/default/bin/nix-daemon" "${out}"
